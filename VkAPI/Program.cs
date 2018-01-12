4a4c83a6d891861141a6a4ea778235ff7fdc5344
Четвериков Илья <ischetverikov@gmail.com>
2018-01-12 07:33:34 +0300

    VkNet has been connected to the project. Wrote authorization.

--- VkAPI/Program.cs
+++ VkAPI/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
 
-namespace VkAPI
+namespace VkApiTest
 {
     class Program
     {
+        const ulong APP_ID = 12345L;
+        const string LOGIN = "79143400153";
+        const string PASSWORD = "Ironman.2209";
+
         static void Main(string[] args)
         {
+            var vk = new VkApi();
+            try
+            {
+                vk.Authorize(new ApiAuthParams()
+                {
+                    Login = LOGIN,
+                    Password = PASSWORD,
+                    ApplicationId = APP_ID
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Authorization has been failed!");
+                Console.WriteLine("Error{0}", e.ToString());
+                return;
+            }
+
+            Console.WriteLine("Authorization passed successfully...");
         }
     }
 }
